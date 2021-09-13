a750914d7198c38bed8f8ec403b7da80e62a10c9
clodon2 <90060114+clodon2@users.noreply.github.com>
2021-09-13 13:08:37 -0400

    Update WhoFeelsLuckyCorey.py

--- WhoFeelsLuckyCorey.py
+++ WhoFeelsLuckyCorey.py
@@ -49,3 +49,65 @@ while asking:
         print("Something happened please try again")
 print(Scores)
 print(Friends)
+
+New Program
+
+# Corey Verkouteren
+# 9/10/21 -
+# Mr Ball's class
+# Dictionary practice
+
+# give option to do 1k spins
+# Who feels lucky?
+
+import random as rdm
+
+
+def FriendAdder(friend):
+    if friend in Friends:
+        print("Repeated name, please retry")
+    else:
+        Friends.append(friend)
+        Scores[friend] = 0
+
+
+Rewards = [1, 2, 3, 2, 5, 10, 1, 1, 2, 3, 5, 3]
+Friends = []
+Scores = {}
+
+Counter = 0
+asking = True
+inputing = True
+
+
+print("Please input 5 of your friend's names (input the first name then press enter, etc.):")
+while inputing:
+    CurrentFriends = len(Friends)
+    if CurrentFriends < 5:
+        FriendAdder(input())
+    else:
+        inputing = False
+
+while asking:
+    answer = input("Would you like to spin the wheel, check the scores, or quit the game?(s,c, or q): ")
+    if answer.lower() == "s":
+        print("")
+        Winner = rdm.choice(Friends)
+        Reward = rdm.choice(Rewards)
+        Scores[Winner] += Reward
+        print(Winner, "got", Reward, "points!")
+        print("")
+    elif answer.lower() == "c":
+        print("")
+        print("The current scores are:")
+        for key in Scores:
+            print(key, "is at", Scores[key])
+            print("")
+    elif answer.lower() == "q":
+        print("")
+        print("Thank you for playing,")
+        asking = False
+    else:
+        print("")
+        print("Something happened please try again")
+        print("")
