72ecf617fab1a670efcb6c69191ef363c1f19d45
clodon2 <90060114+clodon2@users.noreply.github.com>
2021-09-13 13:41:40 -0400

    Update WhoFeelsLuckyCorey.py

--- WhoFeelsLuckyCorey.py
+++ WhoFeelsLuckyCorey.py
@@ -50,14 +50,17 @@ while asking:
 print(Scores)
 print(Friends)
 
+
+
+
+
 New Program
 
 # Corey Verkouteren
-# 9/10/21 -
+# 9/10/21 - 9/13/21
 # Mr Ball's class
 # Dictionary practice
 
-# give option to do 1k spins
 # Who feels lucky?
 
 import random as rdm
@@ -69,13 +72,15 @@ def FriendAdder(friend):
     else:
         Friends.append(friend)
         Scores[friend] = 0
+    # detects repeated names (inputs) and adds new names (inputs) to the dictionary and list
 
 
-Rewards = [1, 2, 3, 2, 5, 10, 1, 1, 2, 3, 5, 3]
+# Main Program
+
+Rewards = [2, 2, 2, 2, 2, 4, 4, 4, 4, 6, 6, 6, 8, 8,  10]
 Friends = []
 Scores = {}
 
-Counter = 0
 asking = True
 inputing = True
 
@@ -87,9 +92,11 @@ while inputing:
         FriendAdder(input())
     else:
         inputing = False
+# Has the user input their friends, and makes it impossible to put in duplicate names
 
 while asking:
     answer = input("Would you like to spin the wheel, check the scores, or quit the game?(s,c, or q): ")
+
     if answer.lower() == "s":
         print("")
         Winner = rdm.choice(Friends)
@@ -97,17 +104,34 @@ while asking:
         Scores[Winner] += Reward
         print(Winner, "got", Reward, "points!")
         print("")
+    # Gives a random friend a random score based on the lists
+
     elif answer.lower() == "c":
         print("")
         print("The current scores are:")
         for key in Scores:
-            print(key, "is at", Scores[key])
+            print(key, "is at", Scores[key], "points")
             print("")
+    # prints the current score individually for each friend
+
     elif answer.lower() == "q":
         print("")
-        print("Thank you for playing,")
+        HighestScore = 0
+        Champion = ""
+        for key in Scores:
+            KeyScore = Scores[key]
+            if KeyScore > HighestScore:
+                HighestScore = KeyScore
+                Champion = key
+            else:
+                continue
+        # finds the highest key value in the Scores dictionary
+        print("Thank you for playing,", Champion, "has won the game")
         asking = False
+    # quits the game and prints the winner
+
     else:
         print("")
-        print("Something happened please try again")
+        print("Something happened, please try again")
         print("")
+    # ends the if/else statements, prevents errors
