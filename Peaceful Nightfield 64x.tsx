2b6ed6949f6afa3ec0bd7bfbd6790bf61bba7f47
clodon2 <90060114+clodon2@users.noreply.github.com>
2022-05-06 13:55:31 -0400

    resource

---
+++ Peaceful Nightfield 64x.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Peaceful Nightfield 64x" tilewidth="64" tileheight="64" tilecount="207" columns="23">
+ <image source="C:/Users/Student/Downloads/download.png" width="1472" height="576"/>
+ <tile id="30">
+  <animation>
+   <frame tileid="28" duration="600"/>
+   <frame tileid="29" duration="600"/>
+   <frame tileid="30" duration="600"/>
+   <frame tileid="29" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="31">
+  <animation>
+   <frame tileid="28" duration="400"/>
+   <frame tileid="29" duration="400"/>
+   <frame tileid="30" duration="400"/>
+   <frame tileid="29" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
